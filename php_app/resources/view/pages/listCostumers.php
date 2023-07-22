087744f9124ff9b0c4b3915e5e3eef41add0a80e
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-07-22 20:21:38 -0300

    att

--- php_app/resources/view/pages/listCostumers.php
+++ php_app/resources/view/pages/listCostumers.php
@@ -16,16 +16,17 @@ ob_start();
     <title>Listagem de Usuarios</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/dashboard/">
-    <link rel="stylesheet" href="views/bootstrap-5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap-5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap-5.2.3/dist/css/pages.css">
+
     <!-- <link rel="stylesheet" href="./bootstrap-5.2.3/dist/css/menu.css"> -->
-    <link rel="stylesheet" href="./views/bootstrap-5.2.3/dist/css/pages.css">
 
 </head>
 
 <body>
 
     <?php
-    require_once ('./../../view/components/navbar.php');
+    require_once ('./components/navbar.php');
     ?>
 
     <div class="container-fluid">
@@ -171,8 +172,8 @@ ob_start();
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="./views/bootstrap-5.2.3/dist/css/bootstrap.css"></script>
-    <script src="./views/bootstrap-5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./bootstrap-5.2.3/dist/css/bootstrap.css"></script>
+    <script src="./bootstrap-5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
     <script src="dashboard.js"></script>
