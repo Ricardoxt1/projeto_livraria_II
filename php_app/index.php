66550ae8c353b32a47f38662dcdfd845a57a1c8f
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-07-21 21:46:00 -0300

    att index

--- php_app/index.php
+++ php_app/index.php
@@ -1,12 +1,24 @@
 <?php 
-
+    
     require __DIR__.'/vendor/autoload.php';
 
+    use \App\Http\Router;
+    use \App\Http\Response;
     use \App\Controller\Pages\Costumer;
-    
-    $obRequest = new \App\Http\Request;
-    
 
-    exit;
 
-    echo Costumer::getCostumer();
+    define('URL', 'http://localhost/php_app');
+
+    $obRouter = new Router(URL);
+
+    //ROTA COSTUMER
+    $obRouter->get('/', [
+        function(){
+            return new Response(200, Costumer::getCostumer());
+        }
+    ]);
+
+    //IMPRIME O RESPONSE DA ROTA
+    $obRouter->run()
+             ->sendResponse();
+             
