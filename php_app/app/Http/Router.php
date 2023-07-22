3caef0ac30f92d4f8f50fb1fae7f9b0161f03366
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-07-21 21:45:37 -0300

    att file router

--- php_app/app/Http/Router.php
+++ php_app/app/Http/Router.php
@@ -46,7 +46,7 @@ class Router
     {
         //INFORMAÇÕES DA URL ATUAL
         $parseUrl = parse_url($this->url);
-
+        
         //DEFINE O PREFIXO
         $this->prefix = $parseUrl['path'] ?? '';
     }
@@ -59,7 +59,7 @@ class Router
      */
     private function addRoute($method, $route, $params = [])
     {
-
+        
         //VALIDAÇÃO DOS PARAMETROS
         foreach ($params as $key => $value) {
             if ($value instanceof Closure) {
@@ -74,6 +74,7 @@ class Router
 
         //ADICIONA A ROTA PARA DENTRO DA CLASSE
         $this->routes[$patternRoute][$method] = $params;
+
     }
 
     /**
@@ -87,13 +88,100 @@ class Router
     }
 
     /**
+     * método responsável por definir uma rota de POST
+     * @param string $route
+     * @param array $params
+     */
+    public function post($route, $params = [])
+    {
+        return $this->addRoute('POST', $route, $params);
+    }
+
+    /**
+     * método responsável por definir uma rota de DELETE
+     * @param string $route
+     * @param array $params
+     */
+    public function delete($route, $params = [])
+    {
+        return $this->addRoute('DELETE', $route, $params);
+    }
+
+
+
+    /** 
+     * método responsável por retornar a URI desconsiderando o prefixo
+     * @return string
+     */
+    private function getUri()
+    {
+        //URI DA REQUEST
+        $uri = $this->request->getUri();
+        
+       
+        //FATIA A URI COM O PREFIXO
+        $xURi = strlen($this->prefix) ? explode($this->prefix, $uri) : [$uri];
+       
+        //remove último prefixo
+        return end($xURi);
+
+        exit;
+    }
+
+    /**
+     * método responsável por retornar os dados da rota atual
+     * @return array
+     */
+    private function getRoute()
+    {
+        //uri
+        $uri = $this->getUri();
+       
+        //method
+        $httpMethod = $this->request->getHttpMethod();
+
+        //valida as rotas
+        foreach ($this->routes as $patternRoute=>$methods) {
+            //verifica se a uri bate o padrão
+            if (preg_match($patternRoute, $uri)) {
+                //verifica o método
+                if ($methods[$httpMethod]) {
+
+                    //retorno dos parámetros da rota
+                    return $methods[$httpMethod];
+                }
+
+                // método não permitido/definido
+                throw new Exception("Método não permitido", 405);
+            }
+        }
+
+        //url não encontrada
+        throw new Exception("Url não encontrada", 404);
+    }
+
+
+    /**
      * método responsável por executar a rota atual
      * @return Response
      */
     public function run()
     {
         try {
-            throw new Exception("Pagina não encontrada", 404);
+
+            $route = $this->getRoute();
+           
+            //VERIFICA O CONTROLADOR
+            if(!isset($route['controller'])){
+                throw new Exception("A Url não pode ser processado", 500);
+            }
+
+            //ARGUMENTOS DA FUNÇÃO
+            $args = [];
+
+            //RETORNA A EXECUÇÃO DA FUNÇÃO
+            return call_user_func_array($route['controller'], $args);
+
         } catch (Exception $e) {
             return new Response($e->getCode(), $e->getMessage());
         }
