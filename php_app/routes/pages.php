057bda4981fd367ad8ec52b24d24aa891f9963b3
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-07-24 11:37:42 -0300

    routes pages conected to Http Routes

--- php_app/routes/pages.php
+++ php_app/routes/pages.php
@@ -5,42 +5,49 @@ use \App\Controller\Pages;
 
 //ROTA COSTUMER
 $obRouter->get('/costumer', [
-    function(){
-        return new Response(200,Pages\Costumer::getCostumer());
+    function () {
+        return new Response(200, Pages\Costumer::getCostumer());
     }
 ]);
 
 //ROTA AUTHOR
 $obRouter->get('/author', [
-    function(){
-        return new Response(200,Pages\Author::getAuthor());
+    function () {
+        return new Response(200, Pages\Author::getAuthor());
     }
 ]);
 
 //ROTA BOOK
 $obRouter->get('/book', [
-    function(){
-        return new Response(200,Pages\Book::getBook());
+    function () {
+        return new Response(200, Pages\Book::getBook());
     }
 ]);
 
 //ROTA EMPLOYEE
 $obRouter->get('/employee', [
-    function(){
-        return new Response(200,Pages\Employee::getEmployee());
+    function () {
+        return new Response(200, Pages\Employee::getEmployee());
     }
 ]);
 
 //ROTA PUBLISHER
 $obRouter->get('/publisher', [
-    function(){
-        return new Response(200,Pages\Publisher::getPublisher());
+    function () {
+        return new Response(200, Pages\Publisher::getPublisher());
     }
 ]);
 
 //ROTA PUBLISHER
 $obRouter->get('/rental', [
-    function(){
-        return new Response(200,Pages\Rental::getRental());
+    function () {
+        return new Response(200, Pages\Rental::getRental());
     }
 ]);
+
+//ROTA DINÂMICA 
+$obRouter->get('/pagina/{idPagina}/{acao}', [
+    function($idPagina, $acao){
+        return new Response(200, 'Pagina' . $idPagina . ' - ' . $acao);
+    }
+]); 
