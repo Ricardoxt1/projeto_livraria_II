64ab220a23f186d14b88b35f05b5e1bcca4ac9d0
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-07-27 18:04:53 -0300

    created db fake

--- php_app/app/Model/Entity/Organization.php
+++ php_app/app/Model/Entity/Organization.php
@@ -3,5 +3,28 @@
 namespace App\Model\Entity;
 
 class Organization{
-    
+
+    /**
+     * id da tabela
+     * @var integer
+     */
+    public $id = 1;
+
+    /**
+     * nome da tabela
+     * @var string
+     */
+    public $name = 'Organization';
+
+    /**
+     * site da tabela
+     * @var string
+     */
+    public $site = 'http://yourdomain.com';
+
+    /**
+     * description da tabela
+     * @var string
+     */
+    public $description = 'lorem ipsum dolor sit am ea commodo od odit aut odit aut odit aut odit aut odit aut odit aut odit aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o subscript aut o';
 }
