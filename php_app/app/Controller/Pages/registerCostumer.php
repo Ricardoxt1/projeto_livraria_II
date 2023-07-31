e88f7cf764d2ac220b1e7b9c6dd2e36c189ea587
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-07-31 12:57:21 -0300

    creation of method responsible for registering costumer

--- php_app/app/Controller/Pages/registerCostumer.php
+++ php_app/app/Controller/Pages/registerCostumer.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Pages;
 
 use \App\Utils\View;
+use \App\Model\Entity\Costumer;
 
 
 class registerCostumer extends registerPage
@@ -23,5 +24,26 @@ class registerCostumer extends registerPage
 
         //retorna a view da pagina
         return parent::getPage('Registro de Usuario',$content);
+    }
+
+    /**
+     * método responsavel por cadastrar um usuario
+     * @return boolean
+     * @param Request $request
+     */
+    public static function insertCostumer($request){
+        //dados do post
+        $postVars = $request->getPostVars();
+       
+        //nova instancia de usuario
+        $obCostumer = new Costumer();
+        $obCostumer->name = $postVars['name'];
+        $obCostumer->cpf = $postVars['cpf'];
+        $obCostumer->phone_number = $postVars['phone_number'];
+        $obCostumer->address = $postVars['address'];
+        $obCostumer->email = $postVars['email'];
+        $obCostumer->cadastrar();
+
+        return self::getRegisterCostumer();
     }
 }
