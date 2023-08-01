81507592bd3a664e79c3366daddbe861d7343651
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-07-31 21:13:28 -0300

    added

--- php_app/index.php
+++ php_app/index.php
@@ -1,27 +1,20 @@
 <?php 
     
-    require __DIR__.'/vendor/autoload.php';
-
+    require __DIR__. '/includes/app.php';
+    
     use \App\Http\Router;
-    use \App\Utils\View;
-    use \WilliamCosta\DotEnv\Environment;
-
-    // carrega variaveis de ambientes
-    Environment::load(__DIR__);
-
-    //dfine a constante url do projeto
-    define('URL', getenv('URL'));
-
-    //define o valor padrão das variavéis
-    View::init([
-        'URL' => URL
-    ]);
 
     //inicia o router
     $obRouter = new Router(URL);
 
     //inclui as rotas de paginas
     include __DIR__.'/routes/pages.php';
+    include __DIR__.'/routes/author.php';
+    include __DIR__.'/routes/book.php';
+    include __DIR__.'/routes/costumer.php';
+    include __DIR__.'/routes/employee.php';
+    include __DIR__.'/routes/publisher.php';
+    include __DIR__.'/routes/rental.php';
 
     //IMPRIME O RESPONSE DA ROTA
     $obRouter->run()
