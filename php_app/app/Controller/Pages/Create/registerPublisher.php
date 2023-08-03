ed1438c8d667bc3ffbcb37992f138a47a92327a4
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-08-02 22:40:29 -0300

    reneaming folder

---
+++ php_app/app/Controller/Pages/Create/registerPublisher.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Pages\Create;
+
+use \App\Utils\View;
+use \App\Model\Entity\Publisher;
+
+class registerPublisher extends registerPage
+{
+
+    /** metodo para envio de dados da pagina registro editora (view)
+     * @return string
+     *  */
+    public static function getRegisterPublisher()
+    {
+
+        $content = View::render('pages/register/registerPublisher', [
+            //view editora
+            'id' => '1',
+            'name' => 'editora ld',
+        ]);
+
+
+        //retorna a view da pagina
+        return parent::getPage('Registro de Editora',$content);
+    }
+
+    /**
+     * método responsavel por cadastrar um editora
+     * @return boolean
+     * @param Request $request
+     */
+    public static function insertPublisher($request){
+        //dados do post
+        $postVars = $request->getPostVars();
+       
+        //nova instancia de editora
+        $obPublisher = new Publisher();
+        $obPublisher->name = $postVars['name'];
+        $obPublisher->cadastrar();
+
+        return self::getRegisterPublisher();
+    }
+}
