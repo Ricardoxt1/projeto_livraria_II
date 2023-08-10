847942e37d270210fa3b787c6d7deb0d98cb8921
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-08-09 23:45:50 -0300

    added restriction for middleware

--- php_app/includes/app.php
+++ php_app/includes/app.php
@@ -6,6 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use \App\Utils\View;
 use \WilliamCosta\DotEnv\Environment;
 use \WilliamCosta\DatabaseManager\Database;
+use \App\Http\Middleware\Queue as MiddlewareQueue;
 
 // carrega variaveis de ambientes
 Environment::load(__DIR__ . '/../');
@@ -25,4 +26,14 @@ define('URL', getenv('URL'));
 //define o valor padrão das variavéis
 View::init([
     'URL' => URL
+]);
+
+//define o mapeamento de middlewares
+MiddlewareQueue::setMap([
+    'maintenance' => \App\Http\Middleware\Maintenance::class
+]);
+
+//define o mapeamento de middlewares padrões executados em todas as rotas
+MiddlewareQueue::setDefault([
+    'maintenance' 
 ]);
